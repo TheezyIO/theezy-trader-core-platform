899faa1339449c79c9384a9ad17cd8d7134ad8b9
CThaw90 <cthaw@mediaocean.com>
2025-02-27 01:47:27 -0500

    Included build script within portfolio package search function

--- portfolio/search/__main__.py
+++ portfolio/search/__main__.py
@@ -1,6 +1,9 @@
 from lib.common import constants
 
-
 def main(args):
     print(args)
     return {'status': 200, 'message': f'Called the {constants.portfolio_label} search function'}
+
+
+if __name__ == '__main__':
+    print(main({}))
