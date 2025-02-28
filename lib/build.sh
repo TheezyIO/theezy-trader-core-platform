3e473d6f0b01371fa67645258a4a3bb658ce706a
CThaw90 <cthaw@mediaocean.com>
2025-02-28 11:28:14 -0500

    Updated build.sh in lib.common module to create test txt file

---
+++ lib/build.sh
@@ -0,0 +1,3 @@
+if [ -f ../portfolio/search/__main__.py ]; then
+  echo Test file content >> ../portfolio/search/test.txt
+fi
--- portfolio/search/__main__.py
+++ portfolio/search/__main__.py
@@ -2,6 +2,14 @@ from lib.common import constants
 
 def main(args):
     print(args)
+    if args.get('checkFile'):
+        try:
+            with open('test.txt', 'r') as txt_file:
+                print('Found file')
+                print(txt_file.read())
+        except FileNotFoundError as e:
+            print('Did not find the file')
+
     return {'status': 200, 'message': f'Called the {constants.portfolio_label} search function'}
 
 
