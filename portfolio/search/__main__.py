a6f87e175c0cede875c9e1d74b83ebf8ceae0e63
CThaw90 <cthaw@mediaocean.com>
2025-02-28 13:46:24 -0500

    Return appropriate http request responses

--- portfolio/search/__main__.py
+++ portfolio/search/__main__.py
@@ -2,7 +2,7 @@ from lib.common import constants
 
 def main(args):
     print(args)
-    return {'status': 200, 'message': f'Called the {constants.portfolio_label} search function'}
+    return {'body': { 'message': f'Called the {constants.portfolio_label} search function'}, 'status': 200 }
 
 
 if __name__ == '__main__':
--- portfolio/view/__main__.py
+++ portfolio/view/__main__.py
@@ -2,7 +2,7 @@ from lib.common import constants
 
 def main(args):
     print(args)
-    return {'status': 200, 'message': f'Called the {constants.portfolio_label} view function'}
+    return {'status': 200, 'body': { 'message': f'Called the {constants.portfolio_label} view function'} }
 
 
 if __name__ == '__main__':
