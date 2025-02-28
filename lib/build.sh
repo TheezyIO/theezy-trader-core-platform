3909a5b69fade7ccc8bac519bc2c08c8bec2eaac
CThaw90 <cthaw@mediaocean.com>
2025-02-28 13:04:27 -0500

    Created another function to test lib/build.sh reusability

--- lib/build.sh
+++ lib/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function copy_lib {
+  cp -R ../lib "../packages/$1/lib"
+}
+
 if [ -f ../packages/portfolio/search/__main__.py ]; then
-  echo Test file content >> ../packages/portfolio/search/test.txt
+  copy_lib portfolio/search
+fi
+
+if [ -f ../packages/portfolio/view/__main__.py ]; then
+  copy_lib portfolio/view
 fi
