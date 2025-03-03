1e10d3cecc8e4571accbac9921928ba44686f0aa
CThaw90 <cthaw@mediaocean.com>
2025-03-02 21:40:45 -0500

    Updated portfolio create function to valid the portfolio request body

--- portfolio/create/__main__.py
+++ portfolio/create/__main__.py
@@ -4,6 +4,13 @@ from lib.services import portfolio
 
 logger = Logger('portfolio.create')
 
+typ_validation_list = [
+    ('name', str),
+    ('description', str),
+    ('minimumDeposit', int),
+    ('maxMembers', int)
+]
+
 def main(args):
     logger.info(f'Function invocation started...')
 
@@ -14,5 +21,17 @@ def main(args):
     if args['http']['method'] != 'POST':
         return {'statusCode': 405, 'body': { 'message': 'Method not allowed'}}
 
-    logger.info(f'Creating portfolio... {args.keys()}')
-    return {'statusCode': 200, 'body': { 'message': 'Portfolio created successfully'}}
+    if not all([v[0] in args and type(args[v[0]]) == v[1] for v in typ_validation_list]):
+        return {'statusCode': 400, 'body': { 'message': 'Missing or invalid parameters'}}
+
+    request_body = {
+        'name': args['name'],
+        'description': args['description'],
+        'minimumDeposit': args['minimumDeposit'],
+        'maxMembers': args['maxMembers']
+    }
+    logger.info(f'Creating portfolio... {request_body}')
+    # portfolio_service = portfolio.PortfolioService(args['http']['headers']['authorization'])
+    # response = portfolio_service.create_portfolio(request_body)
+
+    return {'statusCode': 200, 'body': {'message': 'Portfolio created successfully'}}
