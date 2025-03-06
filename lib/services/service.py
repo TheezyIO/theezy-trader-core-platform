35b93ca189fc3caa6c7b3e43d34de0aae9eba796
CThaw90 <cthaw@mediaocean.com>
2025-03-06 16:51:51 -0500

    Create reusable logic to encapsulate a response status code and removed the Access Control Allow Origin response header

--- lib/services/service.py
+++ lib/services/service.py
@@ -7,6 +7,7 @@ class Service:
 
     def __init__(self, authorization_token):
         self.authorization_token = authorization_token
+        self.status_code = 200
 
     def send_request(self, method, url, params=None, body=None):
         request_url = f'{constants.urls.buildship}{url}'
@@ -14,7 +15,9 @@ class Service:
             'Authorization': self.authorization_token,
             'Content-Type': 'application/json'
         }
-        return requests.request(method, request_url, headers=headers, json=body, params=params).json()
+        response = requests.request(method, request_url, headers=headers, json=body, params=params)
+        self.status_code = response.status_code
+        return response.json()
 
     def get(self, url, params):
         return self.send_request('GET', url, params=params)
@@ -28,14 +31,5 @@ class Service:
     def delete(self, url, params=None):
         return self.send_request('DELETE', url, params=params)
 
-    @staticmethod
-    def send_response(body, status=200):
-        return {
-            'headers': {
-                # This Access Control Allow Origin will need to be set by an environment variable
-                # 'Access-Control-Allow-Origin':'http://localhost:8888,http://127.0.0.1:8888'
-                # 'Access-Control-Allow-Origin': '*'
-            },
-            'statusCode': status,
-            'body': body
-        }
+    def send_response(self, body, status=None):
+        return {'statusCode': status if status else self.status_code, 'body': body}
