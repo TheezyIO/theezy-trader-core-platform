8d84ed3449082f329660a78c529cd9bc0f3b3530
CThaw90 <cthaw@mediaocean.com>
2025-03-08 00:26:59 -0500

    Include the return value within the stock service class

--- lib/services/stock.py
+++ lib/services/stock.py
@@ -5,7 +5,7 @@ from lib.services import service
 class StockService(service.Service):
 
     def get_details(self, stock_id):
-        self.get(f'{constants.urls.stock_details}', {'id': stock_id})
+        return self.get(f'{constants.urls.stock_details}', {'id': stock_id})
 
     def search(self, portfolio_id):
-        self.get(f'{constants.urls.stock_search}', {'portfolioId': portfolio_id})
+        return self.get(f'{constants.urls.stock_search}', {'portfolioId': portfolio_id})
