8b65e687b81a626fbdd73e20280f41d50e341bdf
CThaw90 <cthaw@mediaocean.com>
2025-03-21 18:51:15 -0400

    Move build.sh script into the functions package after library build

--- lib/build.sh
+++ lib/build.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
-function install_function {
+function copy_lib {
   cp -R ../lib "../packages/$1/lib"
-  pip install -r "../packages/$1/lib/requirements.txt"
+  mv "../packages/$1/lib/build.sh" "../packages/$1"
 }
 
 if [ -f ../packages/account-balance/deposit/__main__.py ]; then
-  install_function account-balance/deposit
+  copy_lib account-balance/deposit
 fi
 
 if [ -f ../packages/account-balance/view/__main__.py ]; then
-  install_function account-balance/view
+  copy_lib account-balance/view
 fi
 
 if [ -f ../packages/portfolio/search/__main__.py ]; then
-  install_function portfolio/search
+  copy_lib portfolio/search
 fi
 
 if [ -f ../packages/portfolio/view/__main__.py ]; then
-  install_function portfolio/view
+  copy_lib portfolio/view
 fi
 
 if [ -f ../packages/portfolio/create/__main__.py ]; then
-  install_function portfolio/create
+  copy_lib portfolio/create
 fi
 
 if [ -f ../packages/portfolio/update/__main__.py ]; then
-  install_function portfolio/update
+  copy_lib portfolio/update
 fi
 
 if [ -f ../packages/portfolio/contribute/__main__.py ]; then
-  install_function portfolio/contribute
+  copy_lib portfolio/contribute
 fi
 
 if [ -f ../packages/portfolio/follower/__main__.py ]; then
-  install_function portfolio/follower
+  copy_lib portfolio/follower
 fi
 
 if [ -f ../packages/portfolio/member/__main__.py ]; then
-  install_function portfolio/member
+  copy_lib portfolio/member
 fi
 
 if [ -f ../packages/stock/details/__main__.py ]; then
-  install_function stock/details
+  copy_lib stock/details
 fi
 
 if [ -f ../packages/stock/search/__main__.py ]; then
-  install_function stock/search
+  copy_lib stock/search
 fi
 
 if [ -f ../packages/stock/transaction/__main__.py ]; then
-  install_function stock/transaction
+  copy_lib stock/transaction
 fi
 
 if [ -f ../packages/stock/purchase/__main__.py ]; then
-  install_function stock/purchase
+  copy_lib stock/purchase
 fi
 
 if [ -f ../packages/stock/sell/__main__.py ]; then
-  install_function stock/sell
+  copy_lib stock/sell
 fi
 
 if [ -f ../packages/stock/dailyprice/__main__.py ]; then
-  install_function stock/dailyprice
+  copy_lib stock/dailyprice
 fi
