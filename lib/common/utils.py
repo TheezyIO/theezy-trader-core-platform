2d9c394c24060fb2c15a4604133345be81d81d50
CThaw90 <cthaw@mediaocean.com>
2025-04-21 22:36:25 -0400

    Migrate Stocks - Purchase api from buildship to the stock/purchase digitalocean function

--- lib/common/utils.py
+++ lib/common/utils.py
@@ -1,5 +1,8 @@
+def is_nestable_type(t):
+    return t in [list, dict]
+
 def validate_all_fields(fields, o):
-    return all(field[0] in o and type(o[field[0]]) == field[1] for field in fields)
+    return all(field[0] in o and type(o[field[0]]) == field[1] and (len(field) == 2 or not is_nestable_type(field[1]) or validate_all_fields(field[2], o[field[0]])) for field in fields)
 
 def validate_any_field(fields, o):
-    return any(field[0] in o and type(o[field[0]]) == field[1] for field in fields)
+    return any(field[0] in o and type(o[field[0]]) == field[1] and (len(field) == 2 or not is_nestable_type(field[1]) or validate_any_field(field[2], o[field[0]])) for field in fields)
