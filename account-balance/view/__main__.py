bda8540f01ba2225e21d609194485994241caa8e
Josue Resilien <joshrez20@gmail.com>
2025-04-23 15:26:42 -0400

    Added error handling

--- account-balance/view/__main__.py
+++ account-balance/view/__main__.py
@@ -17,25 +17,36 @@ def main(args):
     if args['http']['method'] != 'GET':
         return {'statusCode': 405, 'body': { 'message': 'Method not allowed'}}
     
-    account_dao = account.AccountDao()
-    account_record = account_dao.get_account_for_user(authorized_user['sub'])
     
-    if not account_record:
-        account_dao.create_account(authorized_user['sub'])
-        return {
-            'statusCode': 200,
-            'body': {
-                'cashBalance': 0,
-                'equityBalance': 0,
-                'timestamp': datetime.now()
+    account_dao = account.AccountDao()
+    
+    try:
+        account_record = account_dao.get_account_for_user(authorized_user['sub'])
+        
+        if not account_record:
+            account_dao.create_account(authorized_user['sub'])
+            return {
+                'statusCode': 200,
+                'body': {
+                    'cashBalance': 0,
+                    'equityBalance': 0,
+                    'timestamp': datetime.now()
+                }
             }
-        }
-    else:
+        else:
+            return {
+                'statusCode': 200,
+                'body': {
+                    'cashBalance': account_record['cash'],
+                    'equityBalance': account_record['equity'],
+                    'timestamp': str(account_record['modified_at'])
+                }
+            }
+    except Exception as e:
         return {
-            'statusCode': 200,
+            'statusCode': 500,
             'body': {
-                'cashBalance': account_record['cash'],
-                'equityBalance': account_record['equity'],
-                'timestamp': str(account_record['modified_at'])
+                'message': f'Internal server error while viewing account for {authorized_user['sub']} : {str(e)}',
+                'status': 'failed'
             }
         }
