38519b31728072f31d07465083697af7ea807763
Josue Resilien <joshrez20@gmail.com>
2025-04-24 22:46:41 -0400

    Added error handling

--- account-balance/deposit/__main__.py
+++ account-balance/deposit/__main__.py
@@ -28,29 +28,40 @@ def main(args):
         return {'statusCode': 400, 'body': { 'message': 'Deposit amount must be greater than 0'}}
     
     account_dao = account.AccountDao()
-    account_record = account_dao.get_account_for_user(authorized_user['sub'])
-    
-    if not account_record:
+
+    try:
+        account_record = account_dao.get_account_for_user(authorized_user['sub'])
+
+        if not account_record:
+            return {
+                'statusCode': 404,
+                'body': {
+                    'message': 'Account Balance record not found',
+                    'status': 'failed'
+                }
+            }
+
+        transaction_body = {
+            'amount': args['amount'],
+            'transaction_type_id': 3, # TODO: Add Transaction Table DAO for dynamic id lookup
+            'account_balance_id': account_record['id']
+        }
+        account_dao.create_transaction(transaction_body)
+        account_dao.update_account({'amount': account_record['cash'] + args['amount']}, account_record['id'])
+
         return {
-            'statusCode': 404,
+            'statusCode': 200,
             'body': {
-                'message': 'Account Balance record not found',
+                'message': 'Successfully deposited funds into account',
+                'status': 'success'
+            }
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': {
+                'message': f"Internal server error while viewing account : {str(e)}",
                 'status': 'failed'
             }
         }
-        
-    transaction_body = {
-        'amount': args['amount'],
-        'transaction_type_id': 3, # TODO: Add Transaction Table DAO for dynamic id lookup
-        'account_balance_id': account_record['id']
-    }
-    account_dao.create_transaction(transaction_body)
-    account_dao.update_account({'amount': account_record['cash'] + args['amount']}, account_record['id'])
-
-    return {
-        'statusCode': 200,
-        'body': {
-            'message': 'Successfully deposited funds into account',
-            'status': 'success'
-        }
-    }
